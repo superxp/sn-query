e1167e62570bf458d8bea41f498d7a306899c412
necho.duan <necho.duan@sunvalley.com.cn>
2022-04-26 19:37:53 +0800

    66

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+    "extends": ["google"],
+    "parserOptions": {
+        "ecmaVersion": 2018
+    },
+    "env": {
+        "es6": true
+    },
+    rules: {
+        "linebreak-style": ["error","windows"],
+    }
+};
--- README.md
+++ README.md
@@ -7,3 +7,12 @@ yarn start
 ###
   dependencies 
   devDependencies : 不会被打进生产的nodemodules
+
+
+
+  electron-builder 打包
+
+  asar 打包后依赖的主要压缩包
+  
+   
+
